3d590a59d66467f877978d2d81d12be3331093bb
kanoonsantikul <nitisantikul@gmail.com>
2017-01-05 21:16:13 +0700

    local data file and new adaline algorithm

--- learning.py
+++ learning.py
@@ -31,3 +31,28 @@ class Perceptron(object):
         self.weight_[1:] += update * training_row
         self.weight_[0] += update
         return update
+
+class AdalineGD(object):
+    def __init__(self, learning_rate, repeat):
+        self.learning_rate = learning_rate
+        self.repeat = repeat
+
+    def fit(self, training_vector, target_value):
+        self.weight_ = numpy.zeros(1 + training_vector.shape[1])
+        self.cost_ = []
+
+        for i in range(self.repeat):
+            output = self.net_input(training_vector)
+            errors = (target_value - output)
+            self.weight_[1:] += self.learning_rate * training_vector.T.dot(errors)
+            self.weight_[0] += self.learning_rate * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+
+        return self
+
+    def net_input(self, training_vector):
+        return numpy.dot(training_vector, self.weight_[1:]) + self.weight_[0]
+
+    def predict(self, training_vector):
+        return numpy.where(self.activation(training_vector) >= 0.0, 1, -1)
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pandas
 import numpy
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
-from my_perceptron import Perceptron
+from learning import Perceptron ,AdalineGD
 
 def main():
     data_frame = pandas.read_csv(
-            'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+            './iris.data',
             header=None)
-    print(data_frame.tail())
 
     y = data_frame.iloc[0:100, 4].values
     y = numpy.where(y == 'Iris-setosa', -1, 1)
     X = data_frame.iloc[0:100, [0,2]].values
 
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+    ada1 = AdalineGD(0.01, 10).fit(X, y)
+    ada2 = AdalineGD(0.0001, 10).fit(X, y)
+    print(ada1.cost_)
+    print(ada2.cost_)
+
     ppn = Perceptron(0.1, 10)
     ppn.fit(X, y)
-    print(ppn.errors_)
 
 if __name__ == '__main__':
     main()
