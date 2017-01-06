508b4579d5829b8fdcbe88e52e4d46cadc170a00
kanoonsantikul <nitisantikul@gmail.com>
2017-01-06 20:52:20 +0700

    New AdalineSGD algorithm

--- learning.py
+++ learning.py
@@ -1,4 +1,5 @@
 import numpy
+from numpy.random import seed
 
 class Perceptron(object):
     def __init__(self, learning_rate, repeat):
@@ -50,6 +51,63 @@ class AdalineGD(object):
             self.cost_.append(cost)
 
         return self
+
+    def net_input(self, training_vector):
+        return numpy.dot(training_vector, self.weight_[1:]) + self.weight_[0]
+
+    def predict(self, training_vector):
+        return numpy.where(self.activation(training_vector) >= 0.0, 1, -1)
+
+class AdalineSGD(object):
+    def __init__(self, learning_rate, repeat, shuffle, random_state):
+        self.learning_rate = learning_rate
+        self.repeat = repeat
+        self.weight_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, training_vector, target_value):
+        self._initialize_weights(training_vector.shape[1])
+        self.cost_ = []
+
+        for i in range(self.repeat):
+            if self.shuffle:
+                training_vector, target_value = self._shuffle(training_vector, target_value)
+            cost = []
+
+            for training_row, target in zip(training_vector, target_value):
+                cost.append(self._update_weight(training_row ,target))
+            avg_cost = sum(cost)/len(target_value)
+            self.cost_.append(avg_cost)
+
+        return self
+
+    def partial_fit(self, training_vector, target_value):
+        if not self.weight_initialized:
+            self._initialize_weights(X.shape[1])
+        if target_value.ravel().shape[0] > 1:
+            for training_row, target in zip(training_vector, target_value):
+                self._update_weight(training_row, target)
+        else:
+            self._update_weight(training_vector, target_value)
+        return self
+
+    def _shuffle(self, training_vector, target_value):
+        r = numpy.random.permutation(len(target_value))
+        return training_vector[r], target_value[r]
+
+    def _initialize_weights(self, len):
+        self.weight_ = numpy.zeros(1 + len)
+        self.weight_initialized = True
+
+    def _update_weight(self, training_row, target):
+        output = self.net_input(training_row)
+        error = (target - output)
+        self.weight_[1:] += self.learning_rate * training_row.dot(error)
+        self.weight_[0] += self.learning_rate * error
+        cost =  0.5 * error**2
+        return cost
 
     def net_input(self, training_vector):
         return numpy.dot(training_vector, self.weight_[1:]) + self.weight_[0]
--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import pandas
 import numpy
 import matplotlib.pyplot as plt
 
-from learning import Perceptron ,AdalineGD
+from learning import Perceptron ,AdalineSGD, AdalineGD
 
 def main():
-    data_frame = pandas.read_csv(
-            './iris.data',
-            header=None)
+    data_frame = pandas.read_csv('./iris.data', header=None)
 
     y = data_frame.iloc[0:100, 4].values
     y = numpy.where(y == 'Iris-setosa', -1, 1)
     X = data_frame.iloc[0:100, [0,2]].values
 
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+    X_std = numpy.copy(X)
+    X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+    X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
     ada1 = AdalineGD(0.01, 10).fit(X, y)
     ada2 = AdalineGD(0.0001, 10).fit(X, y)
+    ada_gd = AdalineGD(0.01, 15).fit(X_std, y)
+    ada_sgd = AdalineSGD(0.01, 15, True, 1).fit(X_std, y)
     print(ada1.cost_)
+    print()
     print(ada2.cost_)
+    print()
+    print(ada_gd.cost_)
+    print()
+    print(ada_sgd.cost_)
 
     ppn = Perceptron(0.1, 10)
     ppn.fit(X, y)
