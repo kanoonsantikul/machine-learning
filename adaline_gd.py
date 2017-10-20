473fe0fe40b42a9f5f97fb18eb9fc475e5535d28
kanoonsantikul <nitisantikul@gmail.com>
2017-10-20 14:50:54 +0700

    add kNN algorithm

--- adaline_gd.py
+++ adaline_gd.py
@@ -24,4 +24,4 @@ class AdalineGD(object):
         return numpy.dot(training_vector, self.weight_[1:]) + self.weight_[0]
 
     def predict(self, training_vector):
-        return numpy.where(self.activation(training_vector) >= 0.0, 1, -1)
+        return numpy.where(self.net_input(training_vector) >= 0.0, 1, -1)
