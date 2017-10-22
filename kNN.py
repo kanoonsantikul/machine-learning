d0efc7a1374adb380bd1bf2dd240ac090663fdac
kanoonsantikul <nitisantikul@gmail.com>
2017-10-23 00:54:35 +0700

    name convention change

--- kNN.py
+++ kNN.py
@@ -1,28 +1,28 @@
 from numpy import *
 import operator
 
-def createDataSet():
+def create_data_set():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
-def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
+def classify_0(in_x, data_set, labels, k):
+    data_size = data_set.shape[0]
 
     # Euclidian distances calculation
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5
+    diff_mat = tile(in_x, (data_size, 1)) - data_set
+    sq_diff_mat = diff_mat**2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances**0.5
 
     # sort distances
-    sortedDistIndicies = distances.argsort()
+    sorted_distances = distances.argsort()
 
     # vote k nearest
-    classCount={}
+    class_count={}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+        vote_label = labels[sorted_distances[i]]
+        class_count[vote_label] = class_count.get(vote_label, 0) + 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
 
-    return sortedClassCount[0][0]
+    return sorted_class_count[0][0]
